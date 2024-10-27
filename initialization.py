b3d30ab3bba95a2775657b972b7e3d7dce14e73c
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-10-27 14:25:40 +0700

    add comments

--- initialization.py
+++ initialization.py
@@ -8,24 +8,26 @@ class BotInitializer:
     def __init__(self):
         # Предобработка и загрузка данных документации
         self.chunks = preproc_text.preproc_docx_file(
-            docx_file_path='documentation/data.docx',
-            output_json='documentation/output.json',
-            data_txt_path='documentation/data.txt'
+            docx_file_path='documentation/data.docx',  # Путь к файлу документации в формате DOCX
+            output_json='documentation/output.json',    # Путь для сохранения предобработанных данных в формате JSON
+            data_txt_path='documentation/data.txt'      # Путь для сохранения текста документации в формате TXT
         )
 
-        # Загрузка структуры документации и часто задаваемых вопросов
-        self.documentation_structure = self.load_json("documentation/output.json")
-        self.questions_answers = self.load_json("documentation/data_quastions.json")
+        # Загрузка структуры документации и часто задаваемых вопросов из JSON-файлов
+        self.documentation_structure = self.load_json("documentation/output.json")  # Загружаем предобработанные данные документации
+        self.questions_answers = self.load_json("documentation/data_quastions.json")  # Загружаем данные о часто задаваемых вопросах
 
-        # Инициализация генератора промптов и модели
+        # Инициализация генератора промптов с предобработанными данными и вопросами/ответами
         self.prompt_generator = PromptGenerator(chunks=self.chunks, qna=self.questions_answers)
+        
+        # Инициализация модели ClioYandex_GPT с использованием токена OAuth и URI модели из конфигурации
         self.model = ClioYandex_GPT(
-            oauth_token=config.YANDEX_PASSPORT_O_AUTH_TOKEN,
-            modelUri=config.MODEL_URI
+            oauth_token=config.YANDEX_PASSPORT_O_AUTH_TOKEN,  # Токен для авторизации в Яндексе
+            modelUri=config.MODEL_URI  # URI для доступа к модели
         )
 
     @staticmethod
     def load_json(filepath):
         """Вспомогательный метод для загрузки JSON-файла."""
-        with open(filepath, "r", encoding="utf-8") as file:
-            return json.load(file)
+        with open(filepath, "r", encoding="utf-8") as file:  # Открытие файла с указанием кодировки UTF-8
+            return json.load(file)  # Загрузка и возврат содержимого файла в виде словаря Python
--- keyboards.py
+++ keyboards.py
@@ -4,6 +4,7 @@ import logging
 from db import UserDB
 
 
+# Настройка логирования для отслеживания информации и ошибок
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -11,56 +12,63 @@ logger = logging.getLogger(__name__)
 class Keyboard:
     @staticmethod
     def create_main_menu():
+        # Создание главного меню с кнопкой "Документация"
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Документация', callback_data='doc'))
         return markup
 
     @staticmethod
     def markup_back():
+        # Создание кнопки "Назад" для возврата к предыдущему меню
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Назад', callback_data='doc'))
         return markup
 
     @staticmethod
     def create_initial_questions_markup(questions_answers):
+        # Создание разметки для первых 5 вопросов из списка вопросов и ответов
         markup = types.InlineKeyboardMarkup()
         for index, item in enumerate(questions_answers[:5]):  # Выводим первые 5 вопросов
             question = item[0]
-            callback_data = f"get_often_question_{index}"
+            callback_data = f"get_often_question_{index}"  # Формирование уникального идентификатора для каждого вопроса
             markup.add(types.InlineKeyboardButton(text=question, callback_data=callback_data))
+        # Добавление кнопки для раскрытия всех вопросов и кнопки "Назад"
         markup.add(types.InlineKeyboardButton(text="Раскрыть все", callback_data="show_all_questions"))
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="doc"))
         return markup
 
     @staticmethod
     def create_all_questions_markup(questions_answers):
+        # Создание разметки для отображения всех вопросов из списка вопросов и ответов
         markup = types.InlineKeyboardMarkup()
         for index, item in enumerate(questions_answers):
             question = item[0]
-            callback_data = f"get_often_question_{index}"
+            callback_data = f"get_often_question_{index}"  # Формирование уникального идентификатора для каждого вопроса
             markup.add(types.InlineKeyboardButton(text=question, callback_data=callback_data))
+        # Добавление кнопки "Назад" для возврата к начальному списку вопросов
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="show_initial_questions"))
         return markup
     
-
     @staticmethod
     def documentation_markup():
+        # Создание разметки с кнопками для работы с документацией
         markup = types.InlineKeyboardMarkup()
-        # Кнопки для документации
+        # Кнопки для взаимодействия с пользователем по вопросам документации
         markup.add(types.InlineKeyboardButton(text="Остались вопросы по документации?", callback_data="ask_question"))
         markup.add(types.InlineKeyboardButton('Частые вопросы', callback_data='often_questions'))
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="start"))
         return markup
     
-    
     @staticmethod
     def ask_question(markup):
+        # Добавление кнопок для выбора темы вопроса и возврата к началу
         markup.add(types.InlineKeyboardButton(text="Другое", callback_data="topic_"))
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="start"))
         return markup
 
     @staticmethod
     def received_answer_markup():
+        # Разметка для отображения после получения ответа на вопрос
         resolution_markup = types.InlineKeyboardMarkup()
         resolution_markup.add(types.InlineKeyboardButton(text="Оценить ответ", callback_data="question_resolved"))
         resolution_markup.add(types.InlineKeyboardButton(text="Задать другой вопрос", callback_data="ask_question"))
@@ -68,6 +76,7 @@ class Keyboard:
     
     @staticmethod
     def get_quality_markup():
+        # Разметка для оценки качества ответа от 1 до 5
         quality_markup = types.InlineKeyboardMarkup()
         quality_markup.add(
             types.InlineKeyboardButton(text="1", callback_data="quality_1"),
